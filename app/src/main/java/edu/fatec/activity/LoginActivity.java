93a59ff5f3123965d8aa5daac4c0c84070badc65
bruhnohc <bruhno.hc@hotmail.com>
2015-10-12 15:02:01 -0300

    Implementação LoginActivity

--- app/src/main/java/edu/fatec/activity/LoginActivity.java
+++ app/src/main/java/edu/fatec/activity/LoginActivity.java
@@ -2,14 +2,12 @@ package edu.fatec.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.telephony.PhoneNumberFormattingTextWatcher;
-import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -22,6 +20,8 @@ import com.example.gqueiroz.androidtcc.R;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import android.widget.Toast;
+
 import edu.fatec.model.Usuario;
 
 public class LoginActivity extends Activity {
@@ -32,51 +32,58 @@ public class LoginActivity extends Activity {
     private EditText senha;
     private String server;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         server = getString(R.string.wstcc);
-        login.setOnClickListener(new View.OnClickListener() {
-                                     @Override
-                                     public void onClick(View v) {
-                                         RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-                                         String url = server + "wstcc/usuarios/loginUsuario";
-                                         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                                                 new Response.Listener<String>() {
-                                                     @Override
-                                                     public void onResponse(String response) {
-                                                         Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
-                                                     }
-                                                 }, new Response.ErrorListener() {
-                                             @Override
-                                             public void onErrorResponse(VolleyError error) {
-                                                 Toast.makeText(getApplicationContext(), "Erro ao se conectar com o WebService:" + error.toString(), Toast.LENGTH_SHORT).show();
-                                             }
-                                         }) {
-                                             @Override
-                                             public byte[] getBody() throws AuthFailureError {
-                                                 return novoUsuario().getBytes();
-                                             }
-                                         };
-                                         queue.add(stringRequest);
-                                     }
-                                 }
-        );
+
+        login = (Button) findViewById(R.id.LoginUsuario);
+
+        login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+
+                String url = server + "wstcc/usuarios/loginUsuario";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                Usuario jsonUsuario = new Gson().fromJson(response, Usuario.class);
+                                Toast.makeText(getApplicationContext(), jsonUsuario.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Erro ao se conectar com o WebService:" + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+                    @Override
+                    public byte[] getBody() throws AuthFailureError {
+                        return JsonUsuario().getBytes();
+                    }
+                };
+                queue.add(stringRequest);
+            }
+        });
+
     }
 
-    public String novoUsuario() {
+    private String JsonUsuario() {
+        email = (EditText) findViewById(R.id.email);
+        senha = (EditText) findViewById(R.id.senha);
         u.setEmail(email.getText().toString());
         u.setSenha(senha.getText().toString());
         Gson gson = new GsonBuilder().create();
         return gson.toJson(u);
     }
 
-    private void findViewsById() {
-        email = (EditText) findViewById(R.id.email);
-        senha = (EditText) findViewById(R.id.senha);
-       }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
