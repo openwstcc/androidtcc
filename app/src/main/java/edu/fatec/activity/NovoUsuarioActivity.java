20ac34fbf6d00d6d46144f6abe15559d855724c1
gabrielqueiroz <gabriel.fatec@hotmail.com>
2015-11-07 12:09:29 -0200

    Pequenas correções em novo usuário

--- app/src/main/java/edu/fatec/activity/NovoUsuarioActivity.java
+++ app/src/main/java/edu/fatec/activity/NovoUsuarioActivity.java
@@ -320,7 +320,7 @@ public class NovoUsuarioActivity extends Activity {
             senhaConfirma.setError("Insira uma senha");
             senhaConfirma.setFocusable(true);
             return false;
-        } else if (!senha.getText().equals(senhaConfirma)) {
+        } else if (!senha.getText().equals(senhaConfirma.getText())) {
             senhaConfirma.setError("Senha não são iguais");
             senha.setFocusable(true);
             return false;
